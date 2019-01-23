f4ae9efadc126380dd5190a3304a68454648e1f4
Brian <brian.n.norman@gmail.com>
2019-01-23 14:31:21 +0000

    Modified to work in a non-blocking way.

--- ZPH01/ZPH01.cpp
+++ ZPH01/ZPH01.cpp
@@ -1,60 +1,86 @@
+/*
+ZPH01.cpp
+
+Non-blocking code for reading the serial data stream from a ZPH01 sensor
+
+*/
+
 #include <ZPH01.h>
+
+ZPH01::ZPH01(Stream & s)
+{
+_ss=&s;
+}
 
 
 //////////////////////////////////////
-bool ZPH01::Validate(uint8_t Checksum)
+bool ZPH01::validate()
 {
 uint8_t tempq=0;
 
 // sum parts 1 to 7
 for(uint8_t j=1;j<8;j++)
 	{
-	tempq+=Data[j];
+	tempq+=rxData[j];
 	}
 tempq=(~tempq)+1;
 
-if (tempq==Checksum) return true;
+if (tempq==rxData[8]) return true;
 return false;
 }
 
 
-//////////////////////////
-bool ZPH01::ReceiveData()
+/*
+loop() should be called from the main program loop()
+it gathers serial data in a non-blocking way
+*/
+void ZPH01::loop()
 {
-// data rate is once per second
-uint32_t Start=millis();
-while (serial.available()==0)
-	{
-	if ((millis()-Start)>=RECV_TIMEOUT)	return false;	// timeout
-	}
-//look for start byte 0x00
-Start=millis();
-while (serial.peek()!=0xFF)
-	{
-	serial.read();
-	if ((millis()-Start)>=RECV_TIMEOUT) return false; // timeout
-	}
-	
-// read 9 characters or timeout
-Start=millis();
-for (uint8_t i=0;i<sizeof ZPH01::Data;i++)
-	{
-	ZPH01::Data[i]=serial.read();
-	while (serial.available()==0)
-		{
-		if ((millis()-Start)>=RECV_TIMEOUT)	return false; // timeout
+if (!_ss->available()) return;
+
+uint8_t ch=_ss->read();
+
+switch (sensorStatus){
+
+	case pendingFF:
+		if (ch!=0xFF) return;
+		count=0;
+		rxData[count++]=ch;
+		sensorStatus=pending18;	// FF should be followed by 0x18
+		break;
+		
+	case pending18:
+		if (ch!=0x18){
+		// try again
+		sensorStatus=pendingFF;
+		return;
 		}
+		rxData[count++]=ch;
+		sensorStatus=reading;
+		
+	case reading:
+		rxData[count++]=ch;
+		if (count<9) return;
+
+		if (validate()){
+			lowPulseRate=rxData[3]+0.1*rxData[4];
+			dataValid=true;
+			}
+			
+		// wait for the next data frame
+		sensorStatus=pendingFF;	
 	}
-	
-// we have received 9 bytes - check the checksum
-if (Validate(Data[8])) return true;
-return false;
 }
+
+
 
 /////////////////////////////////
-float ZPH01::GetLowPercent()
+float ZPH01::getLowPercent()
 {
-if (!ReceiveData()) return -1;	// special case
+// low pulserate is cached on first read
+// and is only updated if the checksum is
+// correct for subsequent reads
+if (!dataValid) return -1.0;	// special case
 
-return ((float)ZPH01::Data[3]+0.1*ZPH01::Data[4]);
+return lowPulseRate;
 }
